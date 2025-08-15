6cad5b403d1c22551569161678f38706c4cbbfb1
Shaamallow <eyal.benaroche@gmail.com>
2025-08-16 00:33:59 +0200

    WIP inherit and cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
 
     # Hardware optimization
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
   };
   outputs =
     {
@@ -14,10 +15,35 @@
       nixpkgs-stable,
       nixos-hardware,
       ...
-    }:
+    }@inputs:
+    let
+      # Supported systems
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      inheritArgs = {
+        inherit
+          forAllSystems
+          inputs
+          nixos-hardware
+          nixpkgs
+          nixpkgs-stable
+          ;
+      };
+    in
     {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      # add Overlays here later
+      # add devshell here later
       nixosConfigurations = {
         nixmaster = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inheritArgs; };
           modules = [
             ./nixos/configuration.nix
             nixos-hardware.nixosModules.framework-13th-gen-intel
