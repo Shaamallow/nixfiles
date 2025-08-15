5876f03c406ebfb0c0f2749d78fd8073d83720e1
Shaamallow <eyal.benaroche@gmail.com>
2025-08-16 00:20:19 +0200

    add hardware optimizations

--- flake.nix
+++ flake.nix
@@ -1,19 +1,28 @@
 {
-  description = "A very basic flake";
+  description = "Shaamallow's Configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    # Hardware optimization
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
   outputs =
     {
       self,
       nixpkgs,
       nixpkgs-stable,
+      nixos-hardware,
       ...
     }:
     {
-      nixosConfigurations.nixmaster = nixpkgs.lib.nixosSystem {
-        modules = [ ./nixos/configuration.nix ];
+      nixosConfigurations = {
+        nixmaster = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./nixos/configuration.nix
+            nixos-hardware.nixosModules.framework-13th-gen-intel
+          ];
+        };
       };
     };
 }
