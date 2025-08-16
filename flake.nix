6306a6c2984a6a4b001cec59bfc67475f01e2bce
Shaamallow <eyal.benaroche@gmail.com>
2025-08-16 13:49:07 +0200

    update nixos version and pkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Shaamallow's Configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # Hardware optimization
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
