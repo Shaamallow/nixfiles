8d30ceffd5d3d5ee6accc29842a13ebc73b11fcc
Shaamallow <eyal.benaroche@gmail.com>
2025-08-17 16:35:33 +0200

    add nextcloud + keyring on boot

--- home-manager/applications/nextcloud.nix
+++ home-manager/applications/nextcloud.nix
@@ -1,10 +1,8 @@
 { pkgs, ... }:
 {
-  services.nextcloud-client.enable = true;
-  # because of https://discourse.nixos.org/t/nextcloud-client-does-not-auto-start-in-gnome3/46492/7
-  systemd.user.services.nextcloud-client = {
-    Unit = {
-      After = pkgs.lib.mkForce "graphical-session.target";
-    };
+  services.nextcloud-client = {
+    enable = true;
+    startInBackground = true;
   };
+
 }
