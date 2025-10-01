3d3762813e328b4b0d74b99e70c80776e01d999d
Shaamallow <eyal.benaroche@gmail.com>
2025-10-01 16:33:25 +0200

    add copilot back to nvim config

---
+++ dotfiles/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup({
+        suggestion = {
+          enable = false,
+        },
+        panel = {
+          enable = false,
+        },
+        filetypes = {
+          markdown = true,
+        },
+      })
+    end,
+  },
+  {
+    'zbirenbaum/copilot-cmp',
+    config = function()
+      require('copilot_cmp').setup()
+    end,
+  },
+}
